d716f266334de2aaf8e6eb19db768e224055825f
manc1n1 <mancinij1111@gmail.com>
2023-11-08 21:17:18 -0500

    Add models

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,52 +1,43 @@
 import { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { useLoginContext, LoginProvider } from '../utils/LoginContext';
 
-
-
 function Navbar() {
-    // useNavigation hook to redirect to logged in after signing up 
-    //const [nav, setNav] = useState(false);
-    //const handleClick = () => setNav(!nav);
-    //const { setLogin, login, loginUser, logoutUser } = useLoginContext();
+	// useNavigation hook to redirect to logged in after signing up
+	//const [nav, setNav] = useState(false);
+	//const handleClick = () => setNav(!nav);
+	//const { setLogin, login, loginUser, logoutUser } = useLoginContext();
 
-    const loggedIn = true;
+	const loggedIn = true;
 
+	function logout() {
+		console.log('logged out successfully');
+	}
 
-    function logout() {
-        console.log("logged out successfully")
-    }
-    
-    return (
-    <div className='container'>
-        <ul className='list-group'>
-            <li>
-            <Link to='home'>
-                Home
-            </Link>
-            </li>
-            <li>
-            <Link to='profile'>
-                Profile
-            </Link>
-            </li>
-            <li>
-            {loggedIn ?     
-                <Link to='dashboard'>My Dashboard</Link>
-                : ""}
-            </li>
-            <li>
-            {loggedIn ?     
-                <button>
-                    <Link to='login'>Login</Link>
-                </button>
-                :
-                <button onClick={() => logoutUser()}>Logout</button>
-            }
-            </li>
-        </ul>
-    </div>
-    );
-};
+	return (
+		<div className="container">
+			<ul className="list-group">
+				<li>
+					<Link to="home">Home</Link>
+				</li>
+				<li>
+					<Link to="profile">Profile</Link>
+				</li>
+				<li>
+					{loggedIn ? <Link to="dashboard">My Dashboard</Link> : ''}
+				</li>
+				<li>
+					{loggedIn ? (
+						<button>
+							<Link to="login">Login</Link>
+						</button>
+					) : (
+						<button onClick={() => logoutUser()}>Logout</button>
+					)}
+				</li>
+			</ul>
+		</div>
+	);
+}
 
 export default Navbar;
