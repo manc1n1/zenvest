d2392dc48284cfdd24d0bf2f3b2f551fed583f06
Sophia Williams <sophiaashleighwilliams@gmail.com>
2023-11-09 21:32:19 -0500

    implemented conditional rendering

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,77 +1,32 @@
-import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import { getMatchup, createVote } from '../utils/api';
-
-// Uncomment import statements below after building queries and mutations
-// import { useQuery, useMutation } from '@apollo/client';
-// import { CREATE_VOTE } from '../utils/mutations';
-// import { QUERY_MATCHUPS } from '../utils/queries';
+import React, { useState } from 'react';
+import { useLoginContext } from '../utils/LoginContext';
 
 const Login = () => {
-	const [matchup, setMatchup] = useState({});
-	let { id } = useParams();
+  const { setLogin, loginUser } = useLoginContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	useEffect(() => {
-		const getMatchupInfo = async () => {
-			try {
-				const res = await getMatchup(id);
-				if (!res.ok) {
-					throw new Error('No matchup');
-				}
-				const matchup = await res.json();
-				setMatchup(matchup);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		getMatchupInfo();
-	}, [id]);
+  const handleLogin = async () => {
+    await loginUser(email, password);
+  };
 
-	const handleVote = async (techNum) => {
-		try {
-			const res = await createVote({ id, techNum });
-
-			if (!res.ok) {
-				throw new Error('Could not vote');
-			}
-
-			const matchup = await res.json();
-			console.log(matchup);
-			setMatchup(matchup);
-		} catch (err) {
-			console.error(err);
-		}
-	};
-
-	return (
-		<div className="card bg-white card-rounded w-50">
-			<div className="card-header bg-dark text-center">
-				<h1>Here is the matchup!</h1>
-			</div>
-			<div className="card-body text-center mt-3">
-				<h2>
-					{matchup.tech1} vs. {matchup.tech2}
-				</h2>
-				<h3>
-					{matchup.tech1_votes} : {matchup.tech2_votes}
-				</h3>
-				<button className="btn btn-info" onClick={() => handleVote(1)}>
-					Vote for {matchup.tech1}
-				</button>{' '}
-				<button className="btn btn-info" onClick={() => handleVote(2)}>
-					Vote for {matchup.tech2}
-				</button>
-				<div className="card-footer text-center m-3">
-					<br></br>
-					<Link to="/">
-						<button className="btn btn-lg btn-danger">
-							View all matchups
-						</button>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
