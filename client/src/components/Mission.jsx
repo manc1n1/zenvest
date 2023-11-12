b1365b4a93fed29dfa084620008a64d1ad662d00
manc1n1 <mancinij1111@gmail.com>
2023-11-12 16:34:00 -0500

    Update homepage

--- client/src/components/Mission.jsx
+++ client/src/components/Mission.jsx
@@ -4,7 +4,7 @@ function Mission() {
 			<div className="flex justify-between text-sm sm:text-2xl">
 				<p>
 					At{' '}
-					<span className="bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-r to-blue-500 via-pink-500 from-violet-500 bg-size-200 hover:bg-right">
+					<span className="bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-r to-blue-300 via-pink-500 from-violet-300 bg-size-200 hover:bg-right font-bold">
 						ZenVest
 					</span>{' '}
 					lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
