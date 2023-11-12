c0a8d019220e482aca63311b8d9e91be29d80284
manc1n1 <mancinij1111@gmail.com>
2023-11-12 05:11:35 -0500

    Update frontend - small changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,12 +30,12 @@ const Login = () => {
 	};
 
 	return (
-		<section className="w-full max-w-xs mx-auto">
+		<section className="w-full max-w-xs p-5 sm:p-0 mx-auto">
 			<form
 				className="h-full w-full rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg px-8 pt-6 pb-8 mb-4"
 				onSubmit={handleFormSubmit}
 			>
-				<div className="mb-4">
+				<div className="mb-4 text-sm sm:text-base">
 					<label
 						className="block font-bold mb-2 text-white text-opacity-60"
 						htmlFor="email"
@@ -54,7 +54,7 @@ const Login = () => {
 						onChange={handleChange}
 					/>
 				</div>
-				<div className="mb-4">
+				<div className="mb-4 text-sm sm:text-base">
 					<label
 						className="block font-bold mb-2 text-white text-opacity-60"
 						htmlFor="password"
@@ -73,15 +73,15 @@ const Login = () => {
 						onChange={handleChange}
 					/>
 				</div>
-				<div className="flex items-center justify-between">
+				<div className="flex items-center justify-between text-sm sm:text-base">
 					<button
-						className="text-white transition-all duration-500 bg-gradient-to-r to-violet-500 via-pink-500 from-blue-500 bg-size-200 hover:bg-right font-bold rounded-lg text-sm py-2 px-4"
+						className="text-white transition-all duration-500 bg-gradient-to-r to-violet-500 via-pink-500 from-blue-500 bg-size-200 hover:bg-right font-bold rounded-lg py-2 px-4"
 						type="submit"
 					>
 						Login
 					</button>
 					<Link
-						className="inline-block align-baseline font-bold text-sm text-white text-opacity-60 hover:text-opacity-100"
+						className="inline-block align-baseline font-bold text-white text-opacity-60 hover:text-opacity-100"
 						to="/signup"
 					>
 						New user? Sign up!
