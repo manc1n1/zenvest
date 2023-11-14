c5a7c1470e10a31e060125fc8263249cdf00f579
manc1n1 <mancinij1111@gmail.com>
2023-11-13 19:38:01 -0500

    Update mission

--- client/src/components/Mission.jsx
+++ client/src/components/Mission.jsx
@@ -18,18 +18,6 @@ function Mission() {
 				</p>
 				<br />
 				<p>
-					At{' '}
-					<span className="bg-clip-text text-transparent transition-all duration-500 bg-gradient-to-r to-blue-300 via-pink-500 from-violet-300 bg-size-200 hover:bg-right font-bold">
-						ZenVest
-					</span>
-					, we understand that the financial landscape can be
-					overwhelming, especially for those who are just beginning
-					their journey into the world of investing. That's why we've
-					created a space that speaks directly to the unique needs and
-					aspirations of Generation Z.
-				</p>
-				<br />
-				<p>
 					Our mission is simple: to empower young individuals with the
 					knowledge and tools they need to make informed and confident
 					investment decisions. We believe that everyone deserves a
